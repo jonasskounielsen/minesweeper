3325a2b9429f94806b63aa4f3759813dc64b3c90
jonas <jonasskou.n@gmail.com>
2025-06-04 21:54:26 +0200

    now counts adjacent mines

--- src/view.rs
+++ src/view.rs
@@ -1,4 +1,4 @@
-use crate::grid::cell::{Cell, CellValue};
+use crate::grid::cell::{Cell, CellState, CellValue};
 use super::{Grid, Place};
 use self::matrix::Matrix;
 
@@ -17,20 +17,18 @@ pub enum ViewCell {
 impl ViewCell {
     pub const fn char(&self) -> &str {
         match *self {
-            ViewCell::Unrevealed => " ",
-            ViewCell::Flagged    => "+",
-            ViewCell::Clear      => "0",
-            ViewCell::Mine       => "*",
-            ViewCell::One        => "1",
-            ViewCell::Two        => "2",
-            ViewCell::Three      => "3",
-            ViewCell::Four       => "4",
-            ViewCell::Five       => "5",
-            ViewCell::Six        => "6",
-            ViewCell::Seven      => "7",
-            ViewCell::Eight      => "8",
+            ViewCell::Unrevealed => " ", ViewCell::One        => "1", ViewCell::Two        => "2",
+            ViewCell::Flagged    => "+", ViewCell::Three      => "3", ViewCell::Four       => "4",
+            ViewCell::Clear      => "0", ViewCell::Five       => "5", ViewCell::Six        => "6",
+            ViewCell::Mine       => "*", ViewCell::Seven      => "7", ViewCell::Eight      => "8",
         }
     }
+}
+
+pub enum MineCount {
+    Zero,
+    One, Two, Three, Four,
+    Five, Six, Seven, Eight,
 }
 
 #[derive(Debug)]
@@ -47,13 +45,9 @@ impl View<'_> {
         let matrix = Matrix::new(
             width, height,
             |relative_x, relative_y| {
-                let width =      TryInto::<i32>::try_into(width     ).unwrap();
-                let height =     TryInto::<i32>::try_into(height    ).unwrap();
-                let relative_x = TryInto::<i32>::try_into(relative_x).unwrap();
-                let relative_y = TryInto::<i32>::try_into(relative_y).unwrap();
                 let cell_position = Place {
-                    x: origin.x + width  / 2 - relative_x - 1,
-                    y: origin.y + height / 2 - relative_y - 1,
+                    x: origin.x + width  as i32 / 2 - relative_x as i32 - 1,
+                    y: origin.y + height as i32 / 2 - relative_y as i32 - 1,
                 };
                 View::get_view_cell(grid, cell_position)
             },
@@ -70,9 +64,38 @@ impl View<'_> {
     fn get_view_cell(grid: &Grid, place: Place) -> ViewCell {
         let cell = grid.get(place);
         match cell {
-            Cell { revealed: false, .. }         => ViewCell::Unrevealed,
-            Cell { value: CellValue::Empty, .. } => ViewCell::Clear,
-            Cell { value: CellValue::Mine,  .. } => ViewCell::Mine,
+            Cell { state: CellState::Hidden,  .. } => ViewCell::Unrevealed,
+            Cell { state: CellState::Flagged, .. } => ViewCell::Flagged,
+            Cell { value: CellValue::Mine,    .. } => ViewCell::Mine,
+            Cell { value: CellValue::Empty,   .. } => match View::mine_count(grid, place) {
+                MineCount::Zero  => ViewCell::Clear,
+                MineCount::One   => ViewCell::One,   MineCount::Two   => ViewCell::Two,
+                MineCount::Three => ViewCell::Three, MineCount::Four  => ViewCell::Four,
+                MineCount::Five  => ViewCell::Five,  MineCount::Six   => ViewCell::Six,
+                MineCount::Seven => ViewCell::Seven, MineCount::Eight => ViewCell::Eight,
+            },
+        }
+    }
+
+    fn mine_count(grid: &Grid, place: Place) -> MineCount {
+        let mut count = 0;
+        for i in -1..1 {
+            for j in -1..1 {
+                if let (0, 0) = (i, j) {
+                    continue;
+                }
+                if let Cell { value: CellValue::Mine, .. } = grid.get(place) {
+                    count += 1;
+                }
+            }
+        }
+        match count {
+            0 => MineCount::Zero,
+            1 => MineCount::One,   2 => MineCount::Two,
+            3 => MineCount::Three, 4 => MineCount::Four,
+            5 => MineCount::Five,  6 => MineCount::Six,
+            7 => MineCount::Seven, 8 => MineCount::Eight,
+            _ => unreachable!(), // we only check 8 tiles
         }
     }
 
